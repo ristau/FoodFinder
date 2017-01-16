94b5586945690a63bfa070e088260efd3e7e6c62
Barbara Ristau <ristaub@gmail.com>
2017-01-16 12:22:16 -0800

    set up YelpClient, Business Model and initial styling of Business Cell

---
+++ FoodFinder/FoodBusinessViewController.swift
@@ -0,0 +1,74 @@
+//
+//  FoodBusinessViewController.swift
+//  FoodFinder
+//
+//  Created by Barbara Ristau on 1/16/17.
+//  Copyright © 2017 FeiLabs. All rights reserved.
+//
+
+import UIKit
+
+class FoodBusinessViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+  var businesses: [Business]?
+  
+  @IBOutlet weak var tableView: UITableView!
+  
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.dataSource = self
+        tableView.delegate = self
+      
+      Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
+        
+        self.businesses = businesses
+        self.tableView.reloadData()
+        if let businesses = businesses {
+          for business in businesses {
+            print(business.name!)
+            print(business.address!)
+          }
+        }
+      }
+      )
+
+        tableView.reloadData()
+    }
+
+ 
+    // MARK: - TableView Functions
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    if businesses != nil {
+      return businesses!.count
+    } else {
+      return 0
+    }
+    
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "BusinessCell", for: indexPath) as! BusinessCell
+    
+    cell.business = businesses?[indexPath.row]
+    
+    return cell
+  }
+  
+  
+  
+  
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FoodFinder/YelpClient.swift
@@ -0,0 +1,92 @@
+//
+//  YelpClient.swift
+//  FoodFinder
+//
+//  Created by Barbara Ristau on 1/16/17.
+//  Copyright © 2017 FeiLabs. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AFNetworking
+import BDBOAuth1Manager
+
+let yelpConsumerKey = "fHIvDY3aTk4OF4wYvpyc1g"
+let yelpConsumerSecret = "_XVdpsVdV3y-C7YKmLRg1JU8Y4c"
+let yelpToken = "uW8YPTUei-LtzYTlu1jAtZccl0Pp9vhD"
+let yelpTokenSecret = "hVtnaBlPn9zbBdK7AtfOjLLplyA"
+
+enum YelpSortMode: Int {
+  case bestMatched = 0, distance, highestRated
+}
+
+class YelpClient: BDBOAuth1RequestOperationManager {
+  
+  var accessToken: String!
+  var accessSecret: String!
+  
+  // MARK: Shared Instance 
+  
+  static let sharedInstance = YelpClient(consumerKey: yelpConsumerKey, consumerSecret: yelpConsumerSecret, accessToken: yelpToken, accessSecret: yelpTokenSecret)
+  
+  required init(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)!
+  }
+  
+  init(consumerKey key: String!, consumerSecret secret: String!, accessToken: String!, accessSecret: String!) {
+    
+    self.accessToken = accessToken
+    self.accessSecret = accessSecret
+    let baseUrl = URL(string: "https://api.yelp.com/v2/")
+    
+    super.init(baseURL: baseUrl, consumerKey: key, consumerSecret: secret)
+    
+    let token = BDBOAuth1Credential(token: accessToken, secret: accessSecret, expiration: nil)
+    
+    self.requestSerializer.saveAccessToken(token)
+  
+  }
+  
+  func searchWithTerm(_ term: String, completion: @escaping ([Business]?, Error?) -> Void) -> AFHTTPRequestOperation {
+
+    return searchWithTerm(term, sort: nil, categories: nil, deals: nil, completion: completion)
+
+  }
+  
+  func searchWithTerm(_ term: String, sort: YelpSortMode?, categories: [String]?, deals: Bool?, completion: @escaping ([Business]?, Error?) -> Void) -> AFHTTPRequestOperation {
+    
+    // For additional parameters, see http://www.yelp.com/developers/documentation/v2/search_api
+    
+    // Default the location to San Francisco
+    
+    var parameters: [String : AnyObject] = ["term": term as AnyObject, "ll": "37.785771, -122.406165" as AnyObject]
+    
+    if sort != nil {
+      parameters["sort"] = sort!.rawValue as AnyObject?
+    }
+    
+    if categories != nil && categories!.count > 0 {
+      parameters["category_filter"] = (categories!).joined(separator: ",") as AnyObject?
+    }
+    
+    if deals != nil {
+      parameters["deals_filter"] = deals! as AnyObject?
+    }
+    
+    print(parameters)
+    
+    return self.get("search", parameters: parameters, success: { (operation: AFHTTPRequestOperation, response: Any) -> Void in
+      
+      if let response = response as? [String: Any] {
+        let dictionaries = response["businesses"] as? [NSDictionary]
+        if dictionaries != nil {
+          completion(Business.businesses(array: dictionaries!), nil)
+        }
+      }
+    }, failure: { (operation: AFHTTPRequestOperation?, error: Error) -> Void in
+      completion(nil, error)
+    })!
+  }
+  
+  
+}
