daed22958dab2439a67008adcd97c6bba9d09a30
Barbara Ristau <ristaub@gmail.com>
2017-01-16 15:53:56 -0800

    Implemented search bar in navigation bar

--- FoodFinder/FoodBusinessViewController.swift
+++ FoodFinder/FoodBusinessViewController.swift
@@ -8,12 +8,15 @@
 
 import UIKit
 
-class FoodBusinessViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class FoodBusinessViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
 
   var businesses: [Business]?
+  var filteredBusinesses: [Business]?
+  var business: Business?
   
   @IBOutlet weak var tableView: UITableView!
   
+  var searchBar: UISearchBar = UISearchBar()
   
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,44 +26,81 @@ class FoodBusinessViewController: UIViewController, UITableViewDataSource, UITab
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 120
       
-      Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
-        
-        self.businesses = businesses
-        self.tableView.reloadData()
-        if let businesses = businesses {
-          for business in businesses {
-            print(business.name!)
-            print(business.address!)
-          }
-        }
-      }
-      )
-
-        tableView.reloadData()
-    }
-
+        self.navigationItem.titleView = searchBar
+        searchBar.sizeToFit()
+        searchBar.delegate = self
+      
+        fetchDataFromNetwork()
+    
+  }
+  
+     
  
     // MARK: - TableView Functions
   
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     
-    if businesses != nil {
-      return businesses!.count
-    } else {
-      return 0
-    }
+    return filteredBusinesses?.count ?? 0
     
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
     let cell = tableView.dequeueReusableCell(withIdentifier: "BusinessCell", for: indexPath) as! BusinessCell
     
-    cell.business = businesses?[indexPath.row]
-    
+    business = filteredBusinesses?[indexPath.row]
+    cell.business = business
+
     return cell
   }
   
   
+  // MARK: - Search Bar 
+  
+  func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+  
+    filteredBusinesses = searchText.isEmpty ? businesses : businesses?.filter({(business: Business) -> Bool in
+      return (business.name!.range(of: searchText, options: .caseInsensitive) != nil)
+    })
+    
+    tableView.reloadData()
+ 
+  }
+  
+  func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+    self.searchBar.showsCancelButton = true
+  }
+  
+  func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+    searchBar.showsCancelButton = false
+    searchBar.text = ""
+    searchBar.resignFirstResponder()
+    fetchDataFromNetwork()
+  }
+  
+  // MARK: - Network Request 
+  
+  func fetchDataFromNetwork() {
+    
+    
+    Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
+      
+      self.businesses = businesses
+      self.filteredBusinesses = businesses
+      self.tableView.reloadData()
+      if let businesses = businesses {
+        for business in businesses {
+          print("Name: \(business.name!)")
+          print("Address: \(business.address!)")
+          print("Categories: \(business.categories!)")
+        }
+      }
+    }
+    )
+    
+    tableView.reloadData()
+    
+  }
   
   
     /*
